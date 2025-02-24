bbe2a6307ac896d91bbf89e8529eac53c2cc944c
Natalia Herrera <nvherreramartinez@gmail.com>
2025-02-24 19:41:18 -0300

    preparando la entrega final

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,14 @@
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { Db } from 'mongodb'
 
 const __filename = fileURLToPath(import.meta.url)
 export const __dirname = join(dirname(__filename) + '../../../')
 
 export const config = {
     dirname: __dirname,
-    PORT: 3000,
+    PORT: 8080,
+    /*Db: {
+        connectionString: `mongodb+srv://nvherreramartinez:MbbnffoUAiAXLDVD@cluster0.t1su7.mongodb.net/coderhouse?retryWrites=true&w=majority&appName=Cluster0`,
+    },*/
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import initApp from './app/index.js';
 import {config} from './config/index.js';
+import { initMongoDBAtlas } from './db/index.js';
 
 const app = initApp()
 
+/*initMongoDBAtlas()*/
 
 const server = app.listen(config.PORT, () => {
     console.info(`Server listen on http://localhost:${config.PORT}`)
